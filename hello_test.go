d36c337ead7d630a9fb4fa1d82ac4068aa166f99
pancham <panchamrb@phonepe.com>
2019-05-11 10:48:14 +0530

    switch statements

--- hello_test.go
+++ hello_test.go
@@ -11,15 +11,33 @@ func TestHello(t *testing.T) {
 		}
 	}
 
-	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+	t.Run("to a person", func(t *testing.T) {
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 		assertCorrectMessage(t, got, want)
 	})
 
-	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+	t.Run("empty string", func(t *testing.T) {
+		got := Hello("", "")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Lauren", "French")
+		want := "Bonjour, Lauren"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Kannada", func(t *testing.T) {
+		got := Hello("Pancham", "Kannada")
+		want := "Namaskara, Pancham"
 		assertCorrectMessage(t, got, want)
 	})
 }
