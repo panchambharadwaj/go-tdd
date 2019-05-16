65b118225c2206494013932792a7c2aaa0db2c8b
pancham <panchamrb@phonepe.com>
2019-05-16 18:12:49 +0530

    for

---
+++ for/repeat.go
@@ -0,0 +1,10 @@
+package iteration
+
+// Repeat returns character repeated 5 times
+func Repeat(character string, repeatCount int) string {
+	var repeated string
+	for i := 0; i < repeatCount; i++ {
+		repeated += character
+	}
+	return repeated
+}
