c37169de5ec99ee07572e40fba85aa797a42a79c
pancham <panchamrb@phonepe.com>
2019-05-17 16:58:35 +0530

    Arrays and Slices

---
+++ arrays/sum.go
@@ -0,0 +1,24 @@
+package main
+
+// Sum calculates the total from an array of numbers
+func Sum(numbers []int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
+}
+
+// SumAllTails calculates the respective sums of every slice tails passed in
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return sums
+}
