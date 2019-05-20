73468a3b3e66becfbe9deb390b6126c6b95cf7a4
pancham <panchamrb@phonepe.com>
2019-05-20 18:35:59 +0530

    Add comments

--- dependency_ingestion/di.go
+++ dependency_ingestion/di.go
@@ -6,10 +6,12 @@ import (
 	"net/http"
 )
 
+// Greet sends a personalised greeting to writer
 func Greet(writer io.Writer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
 }
 
+// MyGreeterHandler says Hello, world over HTTP
 func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
 	Greet(w, "world")
 }
